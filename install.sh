812c64d2a5629b3e2c959db9992588bc3640dcf0
CloudCheng <xiaochengzi0105@gmail.com>
2023-04-15 14:36:39 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ opkg install libustream-openssl ca-bundle kmod-tun
 
 rm -rf *tailscale*
 rm -rf /tmp/*tailscale*
-wget https://raw.fastgit.org/cyz0105/tailscale-openwrt/main/tailscale-openwrt.tgz
+wget https://ghproxy.net/https://raw.githubusercontent.com/cyz0105/tailscale-openwrt/main/tailscale-openwrt.tgz
 tar x -zvC / -f tailscale-openwrt.tgz
 rm tailscale-openwrt.tgz
 
