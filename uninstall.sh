ac320af789b9a85fdd8eccba959eb80212711e75
Cloud Cheng <mops_rocket0k@icloud.com>
2023-08-23 10:46:16 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -6,4 +6,7 @@ rm -rf /etc/init.d/tailscale*
 rm -rf /usr/bin/tailscale*
 rm -rf /tmp/tailscale*
 rm -rf /var/lib/tailscale*
+cd /
+find . -type f -name "*tailscale*" -exec rm -f {} +
+find . -type d -name "*tailscale*" -exec rm -rf {} +
 echo "已清除"
