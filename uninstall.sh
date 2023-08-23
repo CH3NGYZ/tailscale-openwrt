8056db0a70479ee873f30f9a0109e0f1d609f97b
Cloud Cheng <mops_rocket0k@icloud.com>
2023-08-23 16:59:41 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@ rm -rf /etc/init.d/tailscale*
 rm -rf /usr/bin/tailscale*
 rm -rf /tmp/tailscale*
 rm -rf /var/lib/tailscale*
-# cd /
-# find . -type f -name "*tailscale*" -exec rm -f {} +
-# find . -type d -name "*tailscale*" -exec rm -rf {} +
-echo "已清除,强烈推荐重启您的openwrt"
+cd /
+find . -type f -name "*tailscale*" -exec rm -f {} +
+find . -type d -name "*tailscale*" -exec rm -rf {} +
+echo "已清除, 强烈推荐重启您的openwrt"
