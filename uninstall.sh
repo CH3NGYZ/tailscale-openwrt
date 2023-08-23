c54eb38722b191d18caed47f87b5bac528da815b
Cloud Cheng <mops_rocket0k@icloud.com>
2023-08-23 16:58:35 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -9,7 +9,7 @@ rm -rf /etc/init.d/tailscale*
 rm -rf /usr/bin/tailscale*
 rm -rf /tmp/tailscale*
 rm -rf /var/lib/tailscale*
-# cd /
-# find . -type f -name "*tailscale*" -exec rm -f {} +
-# find . -type d -name "*tailscale*" -exec rm -rf {} +
+cd /
+find . -type f -name "*tailscale*" -exec rm -f {} +
+find . -type d -name "*tailscale*" -exec rm -rf {} +
 echo "The remaining files have been cleared, it is highly recommended to restart your openwrt"
