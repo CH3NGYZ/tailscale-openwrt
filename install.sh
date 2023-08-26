407c2890ed1dc35c78d6c988ac9888abe4dfdb83
Cloud Cheng <mops_rocket0k@icloud.com>
2023-08-26 10:10:16 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,13 +12,14 @@ tar x -pzvC / -f tailscale-openwrt.tgz
 
 # 删除安装包
 rm tailscale-openwrt.tgz
-
 # 设定开机启动
 /etc/init.d/tailscale enable
-
-ls /etc/rc.d/S*tailscale*
-
+ls /etc/rc.d/*tailscale*
 #启动
-/etc/init.d/tailscale start
-sleep 5
+# /etc/init.d/tailscale start
+/etc/rc.d/S99tailscale start
+echo "请等待30秒直到服务启动"
+sleep 30
+/etc/init.d/tailscale status
+echo "如果无法登陆, 请运行 tailscaled 命令检查日志"
 tailscale up
