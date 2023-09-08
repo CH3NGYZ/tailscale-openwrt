23e5890eb7d9e6882b57f12d1432c2a4f00bb71c
CH3NG <mops_rocket0k@icloud.com>
2023-09-08 17:18:40 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-
+if [ -e /tmp/tailscaled ]; then
+        echo "存在残留, 请卸载并重启后重试"
+        exit 1
+fi
 # opkg update
 opkg install libustream-openssl ca-bundle kmod-tun
 
@@ -18,9 +21,27 @@ ls /etc/rc.d/*tailscale*
 #启动
 # /etc/init.d/tailscale start
 /etc/rc.d/S99tailscale start
-echo "请等待, 服务正在后台下载Tailscale可执行文件中..."
-sleep 10
-/etc/init.d/tailscale status
-echo "如果无法登陆, 请运行 tailscaled 命令检查日志, 或重新运行 tailscale up"
+echo "请等待, Tailscaled 服务正在后台下载 Tailscale 可执行文件..."
+
+start_time=$(date +%s)
+timeout=60  # 1分钟的超时时间
+
+while true; do
+    if [ -e /tmp/tailscaled ]; then
+        echo "/tmp/tailscaled 存在, 继续"
+        break
+    else
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+        if [ $elapsed_time -ge $timeout ]; then
+            echo "超时，退出脚本,请手动打开系统日志查看失败原因"
+            exit 1
+        else
+            sleep 2
+        fi
+    fi
+done
+
+echo "如果无法登陆, 请运行 '/etc/init.d/tailscale stop && clear && /usr/bin/tailscaled' 命令检查日志, 或重新运行 tailscale up"
 tailscale up
 tailscale up
