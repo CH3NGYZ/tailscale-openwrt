cb091ca50e92393ca185fed42f73dfabc2dc8386
CH3NG <mops_rocket0k@icloud.com>
2023-09-08 17:00:52 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -11,6 +11,7 @@ rm -rf /etc/init.d/tailscale*
 rm -rf /usr/bin/tailscale*
 rm -rf /tmp/tailscale*
 rm -rf /var/lib/tailscale*
+echo "已清除配置文件, 正在查找残余文件, 此步需要的时间较久,请耐心等待"
 cd /
 find . -type f -name "*tailscale*" -exec rm -f {} +
 find . -type d -name "*tailscale*" -exec rm -rf {} +
