c79ed52811db0ce6b2dc5b1276c6ef395c50fcc2
CH3NG <mops_rocket0k@icloud.com>
2023-09-09 08:58:37 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
-
+if [ -e /tmp/tailscaled ]; then
+        echo "存在残留, 请卸载并重启后重试"
+        exit 1
+fi
 # opkg update
 opkg install libustream-openssl ca-bundle kmod-tun
 
 
 # 下载安装包
-wget https://raw.githubusercontent.com/CH3NGYZ/tailscale-openwrt/main/tailscale-openwrt.tgz
+wget --tries=5 -c -t 60 https://ghproxy.com/https://raw.githubusercontent.com/CH3NGYZ/tailscale-openwrt/chinese_mainland/tailscale-openwrt.tgz
 
 # 解压
 tar x -pzvC / -f tailscale-openwrt.tgz
@@ -18,9 +21,27 @@ ls /etc/rc.d/*tailscale*
 #启动
 # /etc/init.d/tailscale start
 /etc/rc.d/S99tailscale start
-echo "Wait 10 seconds until the service starts"
-sleep 10
-/etc/init.d/tailscale status
-echo "If the login fails, run 'tailscaled' command to check the log or re-run 'tailscale up'"
+echo "Please wait, Tailscaled service is downloading the Tailscale executable file in the background......"
+
+start_time=$(date +%s)
+timeout=180  # 3分钟的超时时间
+
+while true; do
+    if [ -e /tmp/tailscaled ]; then
+        echo "/tmp/tailscaled is already exists. Go on"
+        break
+    else
+        current_time=$(date +%s)
+        elapsed_time=$((current_time - start_time))
+        if [ $elapsed_time -ge $timeout ]; then
+            echo "The script has timed out. You need to manually open the system log to view the failure cause..."
+            exit 1
+        else
+            sleep 2
+        fi
+    fi
+done
+
+echo "If you cannot log in, run the '/etc/init.d/tailscale stop && clear && /usr/bin/tailscaled 'command to check the log, or re-run 'tailscale up'..."
 tailscale up
 tailscale up
