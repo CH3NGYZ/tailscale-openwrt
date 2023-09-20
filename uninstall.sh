31786a829ab99731f09c93e364c704d9c22948f8
CH3NG <mops_rocket0k@icloud.com>
2023-09-20 22:44:45 +0800

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,17 +1,35 @@
 #!/bin/sh
 
-/tmp/tailscale down --accept-risk=lose-ssh
-/tmp/tailscale logout
-/etc/init.d/tailscale disable
-/etc/init.d/tailscale stop
-ip link delete tailscale0
-rm -rf /etc/tailscale*
-rm -rf /etc/config/tailscale*
-rm -rf /etc/init.d/tailscale*
-rm -rf /usr/bin/tailscale*
-rm -rf /tmp/tailscale*
-rm -rf /var/lib/tailscale*
-cd /
-find . -type f -name "*tailscale*" -exec rm -f {} +
-find . -type d -name "*tailscale*" -exec rm -rf {} +
-echo "The remaining files have been cleared, it is highly recommended to restart your openwrt"
+cleanup() {
+    echo "The configuration file has been cleared and is looking for residual files. This step will take a long time. Please wait patiently."
+    cd /
+    find . -type f -name "*tailscale*" -exec rm -f {} +
+    find . -type d -name "*tailscale*" -exec rm -rf {} +
+    echo "Cleared, it is highly recommended to restart your OpenWRT"
+}
+
+if [ -e /tmp/tailscaled ]; then
+    echo "File exists, stop the Tailscale service and cleanup"
+    /etc/init.d/tailscale stop
+    /tmp/tailscale down --accept-risk=lose-ssh
+    /tmp/tailscale logout
+    /etc/init.d/tailscale disable
+    rm -rf /etc/tailscale*
+    rm -rf /etc/config/tailscale*
+    rm -rf /etc/init.d/tailscale*
+    rm -rf /usr/bin/tailscale*
+    rm -rf /tmp/tailscale*
+    rm -rf /var/lib/tailscale*
+    ip link delete tailscale0
+    cleanup
+else
+    echo "The file does not exist, cleanup directly"
+    rm -rf /etc/tailscale*
+    rm -rf /etc/config/tailscale*
+    rm -rf /etc/init.d/tailscale*
+    rm -rf /usr/bin/tailscale*
+    rm -rf /tmp/tailscale*
+    rm -rf /var/lib/tailscale*
+    ip link delete tailscale0
+    cleanup
+fi
